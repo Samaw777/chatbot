16979f74ee1f9b62fb17def7eca3238a01b62c18
Marcho Handoko <106444254+Marchosh@users.noreply.github.com>
2023-04-26 23:22:56 -0500

    update UI and model
    UI updated with drop down user name logut, new about page.

--- mainchat/urls.py
+++ mainchat/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('afterlogin/', views.afterlogin, name='mainchat-afterlogin'),
     path('chatbot_response/', csrf_exempt(views.chatbot_response), name='chatbot-response'),
     path('chatpaneltest', views.chatpaneltest, name='chatpaneltest'),
+    path('about', views.about, name='about'),
     path('get_Response', views.get_Response, name='get_Response')
 ]
